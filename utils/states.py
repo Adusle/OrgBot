ea5097a012bc0f856a4b16c56fcad3c534155dfb
ZinerSam <153392751+ZinerSam@users.noreply.github.com>
2023-12-17 01:53:45 +0300

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -3,4 +3,6 @@ from aiogram.fsm.state import StatesGroup, State
 class AdminRegistration(StatesGroup):
     first_name = State()
     second_name = State()
-    
+
+class AdminEv(StatesGroup):
+    name = State()

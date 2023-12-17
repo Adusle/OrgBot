c66c73832ca54bbcae7d2fdbde69b87bb8c91e28
ZinerSam <153392751+ZinerSam@users.noreply.github.com>
2023-12-17 17:15:56 +0300

    Update eventbd.py

--- data_base/eventbd.py
+++ data_base/eventbd.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def create_event_table():
     conn = sqlite3.connect('sql.db')
     cursor = conn.cursor()
@@ -7,12 +8,14 @@ def create_event_table():
             CREATE TABLE IF NOT EXISTS event (
                 id INTEGER PRIMARY KEY,
                 event_name TEXT,
+                user_id TEXT,
                 username TEXT
             )
         ''')
     conn.commit()
     cursor.close()
     conn.close()
+
 
 def add_performance(event_name):
     conn = sqlite3.connect('sql.db')
@@ -22,35 +25,44 @@ def add_performance(event_name):
     cursor.close()
     conn.close()
 
-def add_username(id, username):
+
+def add_user_id(id, user_id):
     conn = sqlite3.connect('sql.db')
     cursor = conn.cursor()
-    cursor.execute('UPDATE event SET username = ? WHERE id = ?',(username, id,))
+    cursor.execute('UPDATE event SET user_id = ? WHERE id = ?', (user_id, id,))
     conn.commit()
     cursor.close()
     conn.close()
 
+
 def remove_performance(id):
     conn = sqlite3.connect('sql.db')
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM event WHERE id = ?',(id,))
+    cursor.execute('DELETE FROM event WHERE id = ?', (id,))
     conn.commit()
     cursor.close()
     conn.close()
+
 
 def print_list():
     conn = sqlite3.connect('sql.db')
     cursor = conn.cursor()
     cursor.execute('SELECT id, event_name, username FROM event')
     all_rows = cursor.fetchall()
-    
-    """
-    while row is not None:
-        print(" ".join([str(c) for c in row]))
-        l = [str(c) for c in row]
-        print(*l)
-        row = cursor.fetchone()
-        """
     cursor.close()
     conn.close()
     return all_rows
+
+def add_username(user_id):
+    conn = sqlite3.connect('sql.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT username FROM users WHERE user_id = ?', (user_id,))
+    row = cursor.fetchone()
+    s = ''
+    for i in row:
+        s = s+i
+    print(s)
+    cursor.execute('UPDATE event SET username = ? WHERE user_id = ?', (s, user_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()

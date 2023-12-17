2ef6f7515feb773f5a48b7352eaeb759158a47fa
ZinerSam <153392751+ZinerSam@users.noreply.github.com>
2023-12-17 13:01:59 +0300

    Update user_commands.py

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -1,11 +1,12 @@
-from aiogram import Router, F , types, Bot
+from aiogram import Router, F , types
 from data_base.bd import get_users_id, add_user
 from keyboards import *
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ReplyKeyboardRemove
 from aiogram.filters import Command , CommandObject, CommandStart
-from utils.states import UserRegistration, UserEv
+from utils.states import UserEv, UserRegistration
+from data_base.eventbd import add_username, print_list
 
 router = Router()
 
@@ -20,18 +21,21 @@ async def add_user_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     user_telegram_id = message.from_user.id
     info = get_users_id(user_telegram_id)
-    if info == None:                        
+    if info == None:
         add_user(user_telegram_id,message.text)
     await message.answer("Регистрация прошла успешно", reply_markup=keyboard.kb)
     await state.clear()
-    
+
+
 @router.message(Command("info"))
 async def cmd_start(message: types.Message, state: FSMContext):
     await message.answer("Что он может? Да нихуя собственно", reply_markup=ReplyKeyboardRemove())
 
 @router.message(F.text.lower()=="мероприятие")
 async def keyboard_us(message: types.Message):
-    await message.answer("Список выступлений", reply_markup=keyboard.event)
+    l = '456'
+    print_list(l)
+    await message.answer(l, reply_markup=keyboard.event)
 
 @router.message(F.text.lower()=="зарегистрироваться")
 async def keyboard_us(message: types.Message, state: FSMContext):
@@ -40,9 +44,10 @@ async def keyboard_us(message: types.Message, state: FSMContext):
 
 @router.message(UserEv.ev)
 async def keyboard_us(message: types.Message, state: FSMContext):
-    await state.update_data(ev = message.text)
+    await state.update_data(ev=message.text)
     username = message.from_user.username
     await message.answer("Вы зарегистрировались", reply_markup=keyboard.event)
+    print(message.text, username)
     add_username(message.text, username)
     await state.clear()
 

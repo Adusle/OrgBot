574b3ccb117fec385acaf057b64441a4b3d3c540
ZinerSam <153392751+ZinerSam@users.noreply.github.com>
2023-12-17 19:35:54 +0300

    Update eventbd.py

--- data_base/eventbd.py
+++ data_base/eventbd.py
@@ -78,3 +78,24 @@ def get_performance_username_id():
     conn.close()
     return data
 
+def replace(num1, num2):
+    conn = sqlite3.connect('sql.db')
+    cursor = conn.cursor()
+    names = ["event_name", "user_id", "username"]
+    for i in names:
+        s1 = ''
+        s2 = ''
+        cursor.execute(f'SELECT {i} FROM event WHERE id = ?', (num1,))
+        row1 = cursor.fetchone()
+        cursor.execute(f'SELECT {i} FROM event WHERE id = ?', (num2,))
+        row2 = cursor.fetchone()
+        for n in row1:
+            s1 = s1+n
+        for n in row2:
+            s2 = s2+n
+        cursor.execute(f'UPDATE event SET {i} = ? WHERE id = ?', (s2, num1,))
+        cursor.execute(f'UPDATE event SET {i} = ? WHERE id = ?', (s1, num2,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+

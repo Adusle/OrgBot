ad4fe2125bba5c4f4ebecea18da5838ec7328b36
ZinerSam <153392751+ZinerSam@users.noreply.github.com>
2023-12-17 13:01:40 +0300

    Update keyboard.py

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -2,16 +2,10 @@ from aiogram import types
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
-button = [[KeyboardButton(text='Список мероприятий')],
-          [KeyboardButton(text='Узнать расписание')],
+button = [[KeyboardButton(text='Мероприятие')],
           [KeyboardButton(text='Пожаловаться')]]
 kb = types.ReplyKeyboardMarkup(keyboard=button,resize_keyboard=True)
 
-event = [[KeyboardButton(text='Мероприятие')],
-            [KeyboardButton(text='Назад')]]
-eventlist = types.ReplyKeyboardMarkup(keyboard=event,resize_keyboard=True)
-
-reg = [[KeyboardButton(text='Регистрация')],
-          [KeyboardButton(text='Пожаловаться')],
-            [KeyboardButton(text='Назад')]]
-regreport = types.ReplyKeyboardMarkup(keyboard=reg,resize_keyboard=True)
+ev = [[KeyboardButton(text='Зарегистрироваться')],
+          [KeyboardButton(text='Назад')]]
+event = types.ReplyKeyboardMarkup(keyboard=ev,resize_keyboard=True)

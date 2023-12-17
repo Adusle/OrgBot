c42dffe7862be3d0bbd7854fe7f1fa62cd223808
ZinerSam <153392751+ZinerSam@users.noreply.github.com>
2023-12-17 17:17:16 +0300

    Update keyboard.py

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -10,12 +10,13 @@ ev = [[KeyboardButton(text='Зарегистрироваться')],
           [KeyboardButton(text='Назад')]]
 event = types.ReplyKeyboardMarkup(keyboard=ev,resize_keyboard=True)
 
-adminbutton = [[KeyboardButton(text='Мероприятие')]]
+adminbutton = [[KeyboardButton(text='Мероприятие админ')]]
 
 adminkeyboard = types.ReplyKeyboardMarkup(keyboard=adminbutton, resize_keyboard=True)
 
 adminbutton2 = [[KeyboardButton(text='Добавить позицию')],
                 [KeyboardButton(text='Удалить позицию')],
                 [KeyboardButton(text='Поменять местами позицим')],
-                [KeyboardButton(text='Оповестить об изменениях')]]
+                [KeyboardButton(text='Оповестить')]]
 adminkeyboard2 = types.ReplyKeyboardMarkup(keyboard=adminbutton2, resize_keyboard=True)
+

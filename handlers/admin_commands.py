7c736de2928f66a130d7670ebd8b0e9b6a15b5a9
Adusle <83882566+Adusle@users.noreply.github.com>
2023-12-18 11:26:23 +0300

    Update admin_commands.py

--- handlers/admin_commands.py
+++ handlers/admin_commands.py
@@ -35,36 +35,36 @@ async def event_admin(message: types.Message):
     await message.answer(text=result_message)
 
 @router.message(F.text.lower() == "добавить позицию")
-async def cmd_start(message: types.Message, state: FSMContext):
+async def add_position(message: types.Message, state: FSMContext):
     await state.set_state(Admin.ev)
     await message.answer("Введите название выступления")
 
 @router.message(Admin.ev)
-async def cmd_start(message: types.Message, state: FSMContext):
+async def add_position_end(message: types.Message, state: FSMContext):
     await state.update_data(ev=message.text)
     await message.answer("Выступление добавлено")
     add_performance(message.text)
     await state.clear()
 
 @router.message(F.text.lower() == "удалить позицию")
-async def cmd_start(message: types.Message, state: FSMContext):
+async def delete_position(message: types.Message, state: FSMContext):
     await state.set_state(Admin.rem)
     await message.answer("Выберите номер выступления")
 
 @router.message(Admin.rem)
-async def cmd_start(message: types.Message, state: FSMContext):
+async def delete_position_end(message: types.Message, state: FSMContext):
     await state.update_data(rem=message.text)
     remove_performance(message.text)
     await message.answer("Выступление удалено")
     await state.clear()
 
 @router.message(F.text.lower() == "поменять местами позиции")
-async def cmd_start(message: types.Message, state: FSMContext):
+async def swap_positions(message: types.Message, state: FSMContext):
     await state.set_state(Admin.red)
     await message.answer("Введите номера позиций")
 
 @router.message(Admin.red)
-async def cmd_start(message: types.Message, state: FSMContext):
+async def swap_position_end(message: types.Message, state: FSMContext):
     await state.update_data(red=message.text)
     s = message.text
     replace(s[0], s[2:])
@@ -73,21 +73,21 @@ async def cmd_start(message: types.Message, state: FSMContext):
 
 
 @router.message(F.text.lower() == "вернуться")
-async def cmd_start(message: types.Message):
-    await message.answer("Ты уже в системе, даун", reply_markup=keyboard.adminkeyboard)
+async def back(message: types.Message):
+    await message.answer("Выберите действие", reply_markup=keyboard.adminkeyboard)
 
 @router.message(F.text.lower() == "оповестить")
-async def cmd_start(message: types.Message):
+async def notify(message: types.Message):
     await message.answer("Оповестить всех или уведомить артиста?", reply_markup=keyboard.adminkeyboard3)
 
 @router.message(F.text.lower() == "оповестить всех")
-async def cmd_start(message: types.Message, bot:Bot):
+async def notify_all(message: types.Message, bot:Bot):
     l = get_performance_username_id()
     for username, id in l:
         await bot.send_message(chat_id=id, text=f"Расписание изменилось, прошу ознакомиться, {username}", reply_markup=keyboard.adminkeyboard2)
 
 @router.message(F.text.lower() == "уведомить артиста")
-async def cmd_start(message: types.Message, state: FSMContext):
+async def notify_one(message: types.Message, state: FSMContext):
     await message.answer("Для того чтобы уведомить артиста напишите номер выступления\n Также можно написать номер следующего выступления, чтобы артист успел подготовиться\n Пример уведомления '1 2' ")
     await message.answer("Список выступлений")
     data = print_list()
@@ -101,7 +101,7 @@ async def cmd_start(message: types.Message, state: FSMContext):
     await state.set_state(Admin.an)
     
 @router.message(Admin.an)
-async def cmd_start(message: types.Message, state: FSMContext, bot:Bot):
+async def notify_one_end(message: types.Message, state: FSMContext, bot:Bot):
     await state.update_data(an=message.text) 
     l = message.text
     if len(l) == 3:
